fc428d1db523bbdc4f6f2fbda0ac80ce888fb592
Srinipriya <srinipriyar.msec@gmail.com>
2018-11-07 23:29:53 -0800

    Added R.

--- Sample/src/SampleStr.java
+++ Sample/src/SampleStr.java
@@ -3,7 +3,7 @@ public class SampleStr {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Srinipriya!");
+		System.out.println("Hello Srinipriya R!");
 	}
 
 }
